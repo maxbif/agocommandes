803a1d470f0cac8486a03fb476dde1b2e4f6383a
maxbif <maxime.biffaud@gmail.com>
2019-03-13 19:47:52 +0100

    crée modele de commande

--- agocommandes/settings.py
+++ agocommandes/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django_countries',
+    'commandes',
     'crispy_forms',
     'base',
     'fournisseurs',
--- commandes/models.py
+++ commandes/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+from django_countries.fields import CountryField
+
+from datetime import datetime
+
+from fournisseurs.models import Fournisseur
+
+class Commande(models.Model):
+    TYPE_CHOICES = (
+        ('IN', 'Initiale'),
+        ('RE', 'Reassort'),
+    )
+
+    fournisseur = models.ForeignKey(Fournisseur, on_delete=models.CASCADE)
+    saison = models.CharField(max_length=4)
+    type_commande = models.CharField(max_length=2, choices=TYPE_CHOICES)
+    référence = models.CharField(max_length=100)
+    date = models.DateField()
+    echeance = models.DateField()
+    montant_ht = models.DecimalField(max_digits=10, decimal_places=2)
+    montant_ttc = models.DecimalField(max_digits=10, decimal_places=2)
+    date_created= models.DateTimeField(default=datetime.now)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.nom
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(string(self.fournisseur.nom)+string(self.saison)+string(self.type_commande)+string(self.référence)[:4])
+        self.montant_ttc = self.montant_ht * (1 + self.fournisseur.taux_tva/100)
+        super(Commande, self).save(*args, **kwargs)
