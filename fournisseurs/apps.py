5c91edd5b004e5d6538c93829f83c795c643c605
Maxime Biffaud <m.biffaud@agence-adequat.local>
2019-03-13 17:16:24 +0100

    ajouté modele Fournisseur et lien vers liste

--- fournisseurs/apps.py
+++ fournisseurs/apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
 
+from .models import Fournisseur
 
 class FournisseursConfig(AppConfig):
     name = 'fournisseurs'
+
+admin.site.register(Fournisseur)
--- fournisseurs/models.py
+++ fournisseurs/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Fournisseur(models.Model):
+    TVA_CHOICES = (
+        ('FR', 'France'),
+        ('UE', 'Union Européenne'),
+        ('IN', 'Import')
+    )
+
+    name = models.CharField(max_length=100)
+    adresse = models.CharField(max_length=100)
+    code_postal = models.CharField(max_length=10)
+    pays = models.CharField(max_length=100)
+    tva = models.CharField(max_length=2, choices=TVA_CHOICES, default='France')
+    contact = models.CharField(max_length=100)
+    mail_contact = models.EmailField()
